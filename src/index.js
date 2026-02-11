34f2416b3952989f376fef9ecf98734ab4932afd
Drslope-99 <enamsifon@gmail.com>
2026-02-11 16:25:37 +0100

    implemented the matches post and get routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { matchesRouter } from "./routes/matches.js";
+
 const app = express();
 const PORT = 8000;
 
@@ -9,6 +11,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "GET request received successfully" });
 });
+
+app.use("/matches", matchesRouter);
 
 // Start server and log URL
 app.listen(PORT, () => {
